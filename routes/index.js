def915d32464794022c6e2f1190258ec6362b124
Jeffrey Yao <jeffreyy@ashn.com>
2018-08-15 16:50:52 -0700

    adding a message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Azure' });
+  var model = {
+    title: 'Personal Project',
+    message: process.env.MESSAGE || "This is development"
+  }
+  res.render('index', model);
 });
 
 module.exports = router;
